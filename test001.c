7e1dcc882e515c8bfbcc89d469385fe902acad11
Chris Tyler <chris@tylers.info>
2025-10-28 10:10:14 -0400

    Adjusted pointer

--- test001.c
+++ test001.c
@@ -4,10 +4,10 @@ int main(int argc, char *argv[]) {
 	int *res;
 	printf("This is a test message.\n");
 	if (argc>1) {
-		res = &argc;
+		res = (void*) 1000000000;
 		printf("Argument count (in addition to binary): %d\n", argc - 1);
 		printf("Pointer address for argc:               %p\n", res);
-		printf("Argument count (via pointer): 		%d\n", argc - 1);
+		printf("Argument count (via pointer): 		%d\n", *res );
 	}
 }
 
