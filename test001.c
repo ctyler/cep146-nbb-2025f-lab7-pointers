1dbab911561e2d2bf599263792351f175263657f
Chris Tyler <chris@tylers.info>
2025-10-28 10:01:46 -0400

    Excluded binary from arg count

--- test001.c
+++ test001.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
 	printf("This is a test message.\n");
 	if (argc>1) {
-		printf("Argument count: %d\n", argc);
+		printf("Argument count (in addition to binary): %d\n", argc - 1);
 	}
 }
 
