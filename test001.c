7ea2f2f615459ac0723e2ef1ee67645c58e212de
Chris Tyler <chris@tylers.info>
2025-10-28 10:13:43 -0400

    Added base message

--- test001.c
+++ test001.c
@@ -2,13 +2,15 @@
 
 int main(int argc, char *argv[]) {
 	int *res;
-	printf("Program was called without any additional arguments.\n");
+	printf("This is a demo of pointer math in C.");
 	if (argc>1) {
 		res = &argc;
 		res += 10000;
 		printf("Argument count (in addition to binary): %d\n", argc - 1);
 		printf("Pointer address for argc:               %p\n", res);
 		printf("Argument count (via pointer): 		%d\n", *res );
+	} else {
+		printf("Program was called without any additional arguments.\n");
 	}
 }
 
