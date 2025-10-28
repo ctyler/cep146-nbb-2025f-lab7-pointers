2a88076e30e33879ee4778e77d951347a0edfed0
Chris Tyler <chris@tylers.info>
2025-10-28 10:11:34 -0400

    Added pointer math

--- test001.c
+++ test001.c
@@ -4,7 +4,8 @@ int main(int argc, char *argv[]) {
 	int *res;
 	printf("This is a test message.\n");
 	if (argc>1) {
-		res = (void*) 1000000000;
+		res = &argc;
+		res += 10000;
 		printf("Argument count (in addition to binary): %d\n", argc - 1);
 		printf("Pointer address for argc:               %p\n", res);
 		printf("Argument count (via pointer): 		%d\n", *res );
